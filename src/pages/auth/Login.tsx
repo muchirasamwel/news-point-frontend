b023c3d22f2c301ea766d160c17a263dcda3193e
muchirasamwel <muchirasamwel@gmail.com>
2023-05-24 23:31:02 +0300

    Login/Register * Link login process with auth hook * Link register process with register hook

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import useAuth from '../../hooks/useAuth'
+import LoginForm from '../../components/forms/LoginForm'
+import { UserLoginObj } from '../../types/FormObj'
 
 type Props = {}
 
 const Login = (props: Props) => {
-  return <div>Login</div>
+  const { login } = useAuth()
+  const loginUser = (user: UserLoginObj) => {
+    login(user)
+  }
+  return (
+    <div className='h-100'>
+      <div className=' h-100 d-flex justify-content-center align-items-center'>
+        <LoginForm handleLogin={loginUser} />
+      </div>
+    </div>
+  )
 }
 
 export default Login
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import RegistrationForm from '../../components/forms/RegistrationForm'
 import { UserRegistrationObj } from '../../types/FormObj'
+import useAuth from '../../hooks/useAuth'
 
 type Props = {}
 
 const Register = (props: Props) => {
-  const resgisterUser = (user: UserRegistrationObj) => {}
+  const { register } = useAuth()
+  const resgisterUser = (user: UserRegistrationObj) => {
+    register(user)
+  }
   return (
     <div className='h-100'>
       <div className=' h-100 d-flex justify-content-center align-items-center'>
